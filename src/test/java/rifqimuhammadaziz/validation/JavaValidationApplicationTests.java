4adaaed99da26a92570e5b9b28fac15503daca52
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-02-23 10:16:16 +0700

    create project & add dependencies

---
+++ src/test/java/rifqimuhammadaziz/validation/JavaValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package rifqimuhammadaziz.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
