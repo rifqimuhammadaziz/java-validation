5b56b1c5d7378316e0ad8c55292ec0205662488f
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-02-25 16:15:40 +0700

    create new Object for Constraint test

---
+++ src/main/java/rifqimuhammadaziz/validation/Person.java
@@ -0,0 +1,39 @@
+package rifqimuhammadaziz.validation;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class Person {
+
+    @Size(max = 20, message = "First Name length max length is 20 characters")
+    @NotBlank(message = "First Name can not blank!")
+    private String firstName;
+
+    @Size(max = 20, message = "Last Name length max length is 20 characters")
+    @NotBlank(message = "Last Name can not blank!")
+    private String lastName;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
