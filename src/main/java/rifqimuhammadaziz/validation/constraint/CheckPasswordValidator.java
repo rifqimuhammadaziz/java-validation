b3622cf990ed3a7249da4c4d7b6a58444cd73314
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-02-28 14:16:46 +0700

    create test Constraint Validator Context

--- src/main/java/rifqimuhammadaziz/validation/constraint/CheckPasswordValidator.java
+++ src/main/java/rifqimuhammadaziz/validation/constraint/CheckPasswordValidator.java
@@ -6,11 +6,32 @@ import rifqimuhammadaziz.validation.Register;
 
 public class CheckPasswordValidator implements ConstraintValidator<CheckPassword, Register> {
 
+    private String messageTemplate;
+
     @Override
-    public boolean isValid(Register register, ConstraintValidatorContext constraintValidatorContext) {
+    public void initialize(CheckPassword constraintAnnotation) {
+        messageTemplate = constraintAnnotation.message();
+    }
+
+    @Override
+    public boolean isValid(Register register, ConstraintValidatorContext context) {
         if (register.getPassword() == null || register.getRetypePassword() == null) return true; // skip validation if null
 
         // password must be equals with retypePassword
-        return register.getPassword().equals(register.getRetypePassword());
+        boolean isValid =  register.getPassword().equals(register.getRetypePassword());
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+
+            context.buildConstraintViolationWithTemplate(messageTemplate)
+                    .addPropertyNode("password")
+                    .addConstraintViolation();
+
+            context.buildConstraintViolationWithTemplate(messageTemplate)
+                    .addPropertyNode("retypePassword")
+                    .addConstraintViolation();
+        }
+
+        return isValid;
     }
 }
